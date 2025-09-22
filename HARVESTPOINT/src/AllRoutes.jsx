66296da1779295f08020bfd4c3a0890446960620
Mitul Bhimani <mitulbhimani281@gmail.com>
2025-09-22 17:06:07 +0530

    whatwedo

--- HARVESTPOINT/src/AllRoutes.jsx
+++ HARVESTPOINT/src/AllRoutes.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
+import WhatWeDo from './Pages/WhatWeDo'
+import HowWeDoIt from './Pages/HowWeDoIt'
 
 function AllRoutes() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/what-we-do" element={<WhatWeDo/>} />
+        <Route path="/how-we-do-it" element={<HowWeDoIt/>} />
       </Routes>
     </div>
   )
